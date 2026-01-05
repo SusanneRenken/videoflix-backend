1310c4d586ae377499e35f50e44471c6c8e64961
SusanneRenken <renken.susanne@gmail.com>
2026-01-05 10:06:09 +0100

    M3: add VideoAdmin class

--- video_app/admin.py
+++ video_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Video
 
-# Register your models here.
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'status', 'created_at')
+    list_filter = ('status', 'category', 'created_at')
+    search_fields = ('title', 'description', 'category')
+    fields = ('created_at', 'title', 'description', 'category', 'original_file')
+    readonly_fields = ('created_at', 'status')
+
+admin.site.register(Video, VideoAdmin)
